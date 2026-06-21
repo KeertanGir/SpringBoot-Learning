96872353a2639c1880a0cb8499d3565bdcf80d34
Keertan Gir <gkeertangirgmail.com>
2026-06-21 14:18:55 +0500

    Start Spring - from Starting, Learn Springboot Start Loose Coupling

---
+++ GameRunner/src/test/java/org/learnspringframework/gamerunner/GameRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learnspringframework.gamerunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
