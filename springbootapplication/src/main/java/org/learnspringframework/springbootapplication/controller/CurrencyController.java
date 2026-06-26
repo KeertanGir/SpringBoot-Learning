11a789ac803fef2fbca80e4bbc9bb785f8c81ce1
Keertan Gir <gkeertangirgmail.com>
2026-06-26 22:01:46 +0500

    I learn that How The Confuguration Class Manage the Multiple Complex Configuration in An SpringBoot application and How the cobination of Configuration class With The SpringFramework Is most Powerfull for the Complex Application Configuration

---
+++ springbootapplication/src/main/java/org/learnspringframework/springbootapplication/controller/CurrencyController.java
@@ -0,0 +1,29 @@
+package org.learnspringframework.springbootapplication.controller;
+
+import org.learnspringframework.springbootapplication.Configurations.CurrencyConfigurationService;
+import org.learnspringframework.springbootapplication.Data.Persons;
+import org.learnspringframework.springbootapplication.service.PersonService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CurrencyController {
+
+    private final CurrencyConfigurationService configurationService;
+
+
+    public CurrencyController(CurrencyConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
+
+    @RequestMapping("/currency-configuration")
+    public CurrencyConfigurationService getUsers(){
+        return configurationService;
+    }
+
+
+
+}
